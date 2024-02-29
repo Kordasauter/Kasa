87a84de436df61824d274204c08731b7928523d4
Kordasauter <machinbidule2014@gmail.com>
2024-02-29 12:00:07 +0100

    liens souligné en fontion de l'url

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import '../../styles/header.scss'
 import logo from '../../assets/logo.svg'
 
 function Header(props) {
+    const location = useLocation()
+    console.log(location.pathname)
     return (
         <div className="header">
             <img src={logo} alt="logo Kasa" />
             <nav>
-                <div>
-                    <Link to="/">Accueil</Link>
-                </div>
-                <div>
-                    <Link to="/about">À propos</Link>
-                </div>
+                {location.pathname === '/' ? (
+                    <div>
+                        <Link className="active" to="/">
+                            Accueil
+                        </Link>
+                    </div>
+                ) : (
+                    <div>
+                        <Link to="/">Accueil</Link>
+                    </div>
+                )}
+                {location.pathname === '/about' ? (
+                    <div>
+                        <Link className="active" to="/about">
+                            À propos
+                        </Link>
+                    </div>
+                ) : (
+                    <div>
+                        <Link to="/about">À propos</Link>
+                    </div>
+                )}
             </nav>
         </div>
     )
